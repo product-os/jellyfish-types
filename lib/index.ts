fc22827c345c8b22a881514cbf32b508a37f327f
Graham McCulloch <admin@grahammcculloch.co.uk>
2021-03-26 09:20:59 +0700

    Add JellyfishError interface
    Change-type: minor
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- lib/index.ts
+++ lib/index.ts
@@ -5,3 +5,17 @@
  */
 
 export * as core from './core';
+
+/**
+ * The interface that all Jellyfish Error classes should
+ * implement.
+ */
+export interface JellyfishError extends Error {
+  /**
+   * True if the error could be expected in normal circumstances.
+   * 
+   * i.e. if expected is true, this error isn't a result of a bug
+   * or an out-of-memory or segmentation-fault error etc.
+   */
+  expected: boolean;
+}
