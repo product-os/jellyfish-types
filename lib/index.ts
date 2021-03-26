f1f034dbc5f3376c11a68c66eceb245d8183fa66
Kakhaber <kaxabernet@gmail.com>
2021-03-26 16:26:12 +0400

    Export JSONSchema
    Change-type: minor

--- lib/index.ts
+++ lib/index.ts
@@ -8,3 +8,4 @@ export * as core from './core';
 export { JellyfishError } from './error';
 export * as queue from './queue';
 export * as worker from './worker';
+export { JSONSchema } from './json-schema';
