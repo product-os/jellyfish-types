9a2097463c318b4a53ece7636a00a90c7e42ad90
Josh Bowling <josh@balena.io>
2021-03-26 11:43:39 +0900

    Update docs
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/index.ts
+++ lib/index.ts
@@ -11,11 +11,11 @@ export * as core from './core';
  * implement.
  */
 export interface JellyfishError extends Error {
-  /**
-   * True if the error could be expected in normal circumstances.
-   * 
-   * i.e. if expected is true, this error isn't a result of a bug
-   * or an out-of-memory or segmentation-fault error etc.
-   */
-  expected: boolean;
+	/**
+	 * True if the error could be expected in normal circumstances.
+	 *
+	 * i.e. if expected is true, this error isn't a result of a bug
+	 * or an out-of-memory or segmentation-fault error etc.
+	 */
+	expected: boolean;
 }
