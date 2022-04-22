d22ee7601db5e92880371211d5684ad0aa243ca3
Ramiro González Maciel <ramirogm@gmail.com>
2022-04-21 21:03:23 -0300

    Add RelationshipContract
    Change-type: patch
    Signed-off-by: Ramiro Gonzalez <ramiro.gonzalez@balena.io>

---
+++ lib/core/contracts/relationship.ts
@@ -0,0 +1,22 @@
+import { Contract } from './contract';
+
+export type RelationshipData = RelationshipDataGeneric &
+	RelationshipDataSpecific;
+
+export type RelationshipDataGeneric = {
+	[k: string]: unknown;
+};
+
+export interface RelationshipDataSpecific {
+	inverseName: string | undefined;
+	title: string;
+	inverseTitle: string | undefined;
+	from: {
+		type: string;
+	};
+	to: {
+		type: string;
+	};
+}
+
+export interface RelationshipContract extends Contract<RelationshipData> {}
